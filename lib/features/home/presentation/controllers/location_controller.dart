0b99a2ebeaeb1b39eb3aaab240f8521ae9ee64d5
VOGLOSSOU <n969601@gmail.com>
2026-04-16 20:40:30 +0100

    bonnnn

--- lib/features/home/presentation/controllers/location_controller.dart
+++ lib/features/home/presentation/controllers/location_controller.dart
@@ -7,12 +7,13 @@ class CityState {
 
   const CityState({required this.city, required this.country, this.cityId});
 
-  String get display => '$city, $country';
+  String get display =>
+      city.isEmpty ? 'Localisation non définie' : '$city, $country';
   String get short => city;
 }
 
 class LocationController extends StateNotifier<CityState> {
-  LocationController() : super(const CityState(city: 'Cotonou', country: 'BJ'));
+  LocationController() : super(const CityState(city: '', country: ''));
 
   void selectCity(String city, String country, {String? cityId}) {
     state = CityState(city: city, country: country, cityId: cityId);
